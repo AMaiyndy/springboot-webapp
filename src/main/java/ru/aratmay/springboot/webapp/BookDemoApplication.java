42e91f579bb468ab1e20f3b03a76e08fba60fd2e
Арат Майынды <aratmay@gmail.com>
2021-01-24 21:50:09 +0300

    war packaging + jsp

--- src/main/java/ru/aratmay/springboot/webapp/BookDemoApplication.java
+++ src/main/java/ru/aratmay/springboot/webapp/BookDemoApplication.java
@@ -2,9 +2,17 @@ package ru.aratmay.springboot.webapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class BookDemoApplication {
+public class BookDemoApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(BookDemoApplication.class);
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(BookDemoApplication.class);
     }
